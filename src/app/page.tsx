295d811b0b54092ba65900eb540fdf802a69da3d
Ali-Armaghan <armaghanbinali@gmail.com>
2026-01-23 14:55:12 +0500

    Type set

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import HeroSection from "./Sections/Hero/Hero";
 import OverviewSection from "./Sections/Overview/OverviewSection";
-import Statics from "./Sections/Statics/statics";
-import StudyArea from "./Sections/StudyArea/studyarea";
+import Statics from "./Sections/Statics/Statics";
+import StudyArea from "./Sections/StudyArea/StudyArea";
 // import Featured from "./Sections/featured/featured";
 export default function Home() {
   return (
